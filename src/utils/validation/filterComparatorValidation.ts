e9642d8ce6213d26cda8f5d917d05b8a6bfa009d
danesh97 <danesh97@students.ubc.ca>
2024-10-31 19:53:21 -0700

    🐛Fixing & adding some validation checks

--- src/utils/validation/filterComparatorValidation.ts
+++ src/utils/validation/filterComparatorValidation.ts
@@ -12,33 +12,35 @@ export function validateLogicFilters(logicFilter: any): void {
 
 // For validating Comparators
 export function validateNonLogicFilters(key: any, val: any): void {
-	const [[objectKey], [objectValue]] = [Object.keys(val), Object.values(val)];
-
 	switch (key) {
-		case "NOT":
+		case "NOT": {
 			validateLogicFilters(val);
 			break;
+		}
 		case "EQ":
 		case "GT":
 		case "LT": {
+			const [[objectKey], [objectValue]] = [Object.keys(val), Object.values(val)];
 			validateNumericComparators(objectKey, objectValue);
 			break;
-		}
+			}
 		case "MAX":
 		case "MIN":
 		case "AVG":
-		case "SUM":
+		case "SUM": {
 			validateApplyTokens(key, val);
 			break;
+		}
 		case "IS": {
+			const stringValue = Object.values(val)[0];
 			// Check that IS value is a string type
-			if (typeof objectValue !== "string") {
+			if (typeof stringValue !== "string") {
 				throw new InsightError("Invalid Value type for IS. Value type must be a string");
 			}
 
 			// Check that if wildcards are used, they are not embedded
-			if (objectValue.includes("*")) {
-				if ((!objectValue.startsWith("*") && !objectValue.endsWith("*")) || objectValue === "***") {
+			if (stringValue.includes("*")) {
+				if ((!stringValue.startsWith("*") && !stringValue.endsWith("*")) || stringValue === "***") {
 					throw new InsightError("Invalid use of Wildcard. Wildcard cannot be embedded in a word.");
 				}
 			}
@@ -84,11 +86,11 @@ function validateApplyTokens(objectKey: any, objectValue: any): void {
 	const validTokenValues = ["lat", "lon", "seats", "year", "avg", "pass", "fail", "audit"];
 
 	if (!objectKey) {
-		throw new InsightError("Missing key for EQ, GT, or LT");
+		throw new InsightError("Missing key for MAX, MIN, AVG, or SUM.");
 	}
 
 	// Checking that MAX, MIN, SUM, AVG value column is a numeric type
 	if (!validTokenValues.includes(objectValue.split("_")[1])) {
-		throw new InsightError("Invalid Value type for MAX, MIN, AVG, SUM. Value type must be numeric column");
+		throw new InsightError("Invalid Value type for MAX, MIN, AVG, or SUM. Value type must be numeric column");
 	}
 }
