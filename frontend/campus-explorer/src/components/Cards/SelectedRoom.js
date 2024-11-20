a7d5d543a9c6fefbf296eccf80c03bd3c4974223
danesh97 <danesh97@students.ubc.ca>
2024-11-20 14:58:20 -0800

    Updating Selected room card to include more info

--- frontend/campus-explorer/src/components/Cards/SelectedRoom.js
+++ frontend/campus-explorer/src/components/Cards/SelectedRoom.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function SelectedRoom({room}) {
 	return (
-		<div className="card mb-2">
-			<div className="card-body p-2">
-				<h6 className="card-title">
+		<div className="card mb-2 w-100">
+			<div className="card-body p-1">
+				<h6 className="card-title small mb-1">
 					<a
 						href={`https://learningspaces.ubc.ca/classrooms/${room.rooms_shortname}-${room.rooms_number}`}
 						target="_blank"
@@ -13,11 +13,20 @@ function SelectedRoom({room}) {
 						{room.rooms_name.replace("_", " ")}
 					</a>
 				</h6>
-				<p className="card-text mb-1">
-					Capacity: {room.rooms_seats}
+				<p className="card-text small mb-0">
+					Full Name: {room.rooms_fullname}
 				</p>
-				<p className="card-text mb-1">
-					Type: {room.rooms_type}
+				<p className="card-text small mb-0">
+					Short Name: {room.rooms_shortname}
+				</p>
+				<p className="card-text small mb-0">
+					Number: {room.rooms_number}
+				</p>
+				<p className="card-text small mb-0">
+					Address: {room.rooms_address}
+				</p>
+				<p className="card-text small mb-0">
+					Capacity: {room.rooms_seats}
 				</p>
 			</div>
 		</div>
