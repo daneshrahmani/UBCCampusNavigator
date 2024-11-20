00d69d675d06597402aabceac8e1ddc32eab5568
danesh97 <danesh97@students.ubc.ca>
2024-11-20 14:59:28 -0800

    🌎 Adjusting margins on map

--- frontend/campus-explorer/src/components/UBCMap.js
+++ frontend/campus-explorer/src/components/UBCMap.js
@@ -15,14 +15,16 @@ const UBCMap = () => {
 	};
 
 	return (
-		<LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY || "AIzaSyCZb61HwSZY7b6X558GbsxCxI9-8P-_8Is"}>
-			<GoogleMap
-				mapContainerStyle={mapStyles}
-				zoom={16}
-				center={center}
-			>
-			</GoogleMap>
-		</LoadScript>
+		<div style={{ height: '100%', width: '100%' }}>
+			<LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY || "AIzaSyCZb61HwSZY7b6X558GbsxCxI9-8P-_8Is"}>
+				<GoogleMap
+					mapContainerStyle={mapStyles}
+					zoom={16}
+					center={center}
+				>
+				</GoogleMap>
+			</LoadScript>
+		</div>
 	);
 };
 
