d6ee4eb3bcfa4e782ce0f1fb1e7f60521a61224a
Michael Paknys <mpaknys@students.ubc.ca>
2024-11-21 14:49:48 -0800

    building info panel displays when marker is clicked

--- frontend/campus-explorer/src/components/UBCMap.js
+++ frontend/campus-explorer/src/components/UBCMap.js
@@ -1,4 +1,4 @@
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, InfoBox, LoadScript, Marker } from '@react-google-maps/api';
 import React, { useState } from 'react';
 
 const mapOptions = {
@@ -19,7 +19,10 @@ const UBCMap = ({ states }) => {
 
 	for (let building of roomsByBuilding) {
 		buildingInfo.push({
+			fullname: building[0].rooms_fullname,
 			shortname: building[0].rooms_shortname,
+			address: building[0].rooms_address,
+			roomCount: building.length,
 			lat: building[0].rooms_lat,
 			lon: building[0].rooms_lon
 		})
@@ -30,6 +33,8 @@ const UBCMap = ({ states }) => {
 		lat: 49.26596405700797,
 		lng: -123.25266284768082
 	})
+
+	const [selectedBuilding, setSelectedBuilding] = useState(null)
 
 	const mapStyles = {
 		height: "89%",
@@ -44,14 +49,35 @@ const UBCMap = ({ states }) => {
 					zoom={15.8}
 					center={center}
 					options={mapOptions}
+					onClick={() => setSelectedBuilding(null)}
 				>
 					{buildingInfo.map(b => (
 						<Marker
 							position={{ lat: b.lat, lng: b.lon }}
 							label={b.shortname}
 							icon={null}
-							onClick={() => setCenter({lat: b.lat, lng: b.lon})}
-						/>))}
+							onClick={() => {
+								setCenter({lat: b.lat, lng: b.lon})
+								setSelectedBuilding(b.shortname)
+							}}
+						/>
+					))}
+					{buildingInfo.map(b => {
+						if (b.shortname === selectedBuilding) {
+							return (
+								<InfoBox
+									position={{ lat: b.lat, lng: b.lon }}
+									options={{closeBoxURL: ""}}
+								>
+									<div className="card">
+										<p>{`${b.fullname} (${b.shortname})`}</p>
+										<p>{b.address}</p>
+										<p>Rooms Matching Filters: {b.roomCount}</p>
+									</div>
+								</InfoBox>
+							)
+						}
+					})}
 				</GoogleMap>
 			</LoadScript>
 		</div>
