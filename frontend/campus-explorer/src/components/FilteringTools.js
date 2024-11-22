77423ab6bacdf762e04d4fbd3c755c42d6f52348
danesh97 <danesh97@students.ubc.ca>
2024-11-21 18:01:05 -0800

    ✨ Adding clear filters button

--- frontend/campus-explorer/src/components/FilteringTools.js
+++ frontend/campus-explorer/src/components/FilteringTools.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import CapacitySlider from "./CapacitySlider";
+import ClearFiltersButton from "./Buttons/ClearFilters";
 
 export default function FilteringTools({ states }) {
 
     const handleRoomTypeChange = (e) => {
-        if (e.target.value != "Any") {
+        if (e.target.value !== "Any") {
             states.setSelectedRooms(prevSelectedRooms => prevSelectedRooms.filter(room => room.rooms_type === e.target.value));
         }
         states.setFilters(prevFilters => ({...prevFilters, roomType: e.target.value}))
@@ -20,19 +21,22 @@ export default function FilteringTools({ states }) {
     const roomTypes = [...roomTypesSet].sort();
 
     return (
-        <div className="card p-3">
-            <h5>Filters</h5>
-            <label className="d-flex flex-row align-items-center mb-3 text-nowrap">
-                Room Type:
-                <Form.Select
-                    value={states.filters.roomType}
-                    onChange={e => handleRoomTypeChange(e)}
-                >
-                    <option>Any</option>
-                    {roomTypes.map(type => <option value={type}>{type}</option>)}
-                </Form.Select>
-            </label>
-            <CapacitySlider states={states} />
-        </div>
-    )
+		<div className="card p-3">
+			<div className="d-flex justify-content-between align-items-center mb-3">
+				<h5 className="mb-0">Filters</h5>
+				<ClearFiltersButton states={states}/>
+			</div>
+			<label className="d-flex flex-row align-items-center mb-3 text-nowrap">
+				Room Type:
+				<Form.Select
+					value={states.filters.roomType}
+					onChange={e => handleRoomTypeChange(e)}
+				>
+					<option>Any</option>
+					{roomTypes.map(type => <option value={type}>{type}</option>)}
+				</Form.Select>
+			</label>
+			<CapacitySlider states={states}/>
+		</div>
+	)
 }
